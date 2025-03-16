78ee0a6d20cc44cde2a986af2a4a34506ec7f4c8
Gregory Carlin <gregcarlin@meta.com>
2025-03-16 14:48:00 -0400

    Support availability

--- optimeyes.py
+++ optimeyes.py
@@ -2,52 +2,57 @@ from problem import Problem, new_integer_variable, new_binary_variable, new_cont
 from functools import reduce
 import math
 
-NUM_WEEKS = 4
-# TODO: incorporate availability
 RESIDENT_AVAILABILITY = {
     "Sophia": [
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
+        0, 0, 0, 0, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1,
     ],
     "Paris": [
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
+        1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1,
     ],
     "Keir": [
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1],
+        1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1,
     ],
 }
 
-MAX_DAYS_PER_RESIDENT = math.ceil(NUM_WEEKS * 7 / float(len(RESIDENT_AVAILABILITY.keys())))
+# TODO: validate input
+
+NUM_DAYS = len(next(iter(RESIDENT_AVAILABILITY.values())))
+
+MAX_DAYS_PER_RESIDENT = math.ceil(NUM_DAYS / float(len(RESIDENT_AVAILABILITY.keys())))
 
 DAYS_OF_WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 def var_sum(vs):
     return reduce(lambda x, y: x + y, vs)
 
-def key_for_day(week: int, day: int, resident: str) -> str:
-    return f"Week_{week}_{day}_{resident}"
+def key_for_day(day: int, resident: str) -> str:
+    return f"Day_{day}_{resident}"
 
 problem = Problem("optimEYES", minimize=True)
 
 # For each resident, create a variable representing every day
 day_vars = {resident: [] for resident in RESIDENT_AVAILABILITY.keys()}
-for resident in RESIDENT_AVAILABILITY.keys():
-    for week in range(NUM_WEEKS):
-        for day in DAYS_OF_WEEK:
-            day_vars[resident].append(new_binary_variable(key_for_day(week, day, resident)))
+for resident, availability in RESIDENT_AVAILABILITY.items():
+    for day, is_available in enumerate(availability):
+        day_var = new_binary_variable(key_for_day(day, resident))
+        day_vars[resident].append(day_var)
+        if is_available == 0:
+            # Resident is unavailable this day
+            problem.add_constraint(day_var == 0)
 
     # Ensure even distribution
     problem.add_constraint(var_sum(day_vars[resident]) <= MAX_DAYS_PER_RESIDENT)
 
-for day in range(NUM_WEEKS * 7):
+for day in range(NUM_DAYS):
     all_residents_for_day = [days_for_resident[day] for days_for_resident in day_vars.values()]
     # Ensure exactly one resident is assigned to each day
     problem.add_constraint(var_sum(all_residents_for_day) == 1)
@@ -74,16 +79,14 @@ solution = problem.solve()
 print("Status:", solution.get_status())
 
 results = solution.get_variables()
-for week in range(NUM_WEEKS):
-    print(f"Week {week}:")
-    for day in DAYS_OF_WEEK:
-        assigned_resident = None
-        for resident in RESIDENT_AVAILABILITY.keys():
-            if results[key_for_day(week, day, resident)] != 0.0:
-                assert assigned_resident is None, "Two residents assigned to the same day"
-                assigned_resident = resident
-        assert assigned_resident is not None, "No residents assigned to a day"
-        print(f"\t{day}: {assigned_resident}")
+for day in range(NUM_DAYS):
+    assigned_resident = None
+    for resident in RESIDENT_AVAILABILITY.keys():
+        if results[key_for_day(day, resident)] != 0.0:
+            assert assigned_resident is None, "Two residents assigned to the same day"
+            assigned_resident = resident
+    assert assigned_resident is not None, "No residents assigned to a day"
+    print(f"\t{day}: {assigned_resident}")
 
 """
 for name, val in results.items():
